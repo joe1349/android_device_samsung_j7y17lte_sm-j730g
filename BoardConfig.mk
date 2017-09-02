af43305a5dba8193d5b46187c6487cae788d90d2
joephyu <aacc1349@gmail.com>
2017-09-02 10:40:48 +0800

    first commit

---
+++ BoardConfig.mk
@@ -0,0 +1,105 @@
+#USE_CAMERA_STUB := true
+
+LOCAL_PATH := device/samsung/j7y17lte
+
+# Include path
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := universal7870
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# Platform
+TARGET_BOARD_PLATFORM := exynos5
+TARGET_SLSI_VARIANT := cm
+TARGET_SOC := exynos7880
+TARGET_BOOTLOADER_BOARD_NAME := universal7870
+TARGET_BOARD_PLATFORM_GPU := mali-T830MP3
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 := arm64-v8a
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_SMP := true
+
+# For arm64
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a15
+
+# Binder
+TARGET_USES_64_BIT_BINDER := true
+
+# Kernel
+BOARD_KERNEL_CMDLINE := # Exynos doesn't take cmdline arguments from boot image
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board SRPQA18A000KU --dt $(LOCAL_PATH)/dt.img
+BOARD_VENDOR := samsung
+TARGET_USES_UNCOMPRESSED_KERNEL := true
+TARGET_PREBUILT_KERNEL := device/samsung/j7y17lte/kernel
+
+# DTB
+TARGET_PREBUILT_DTB := device/samsung/j7y17lte/dt.img
+TARGET_DTBH_PLATFORM_CODE := 0x000050a6
+TARGET_DTBH_SUBTYPE_CODE  := 0x217584da
+
+# Partition info
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x2000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x2600000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0xC0000000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x663FFB000
+BOARD_CACHEIMAGE_PARTITION_SIZE := 0xC800000
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+# Use this flag if the board has a ext4 partition larger than 2gb
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+
+# Display
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+#####################################################################################
+# Recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_FSTAB := device/samsung/j7y17lte/recovery/recovery.fstab
+
+RECOVERY_SDCARD_ON_DATA := true
+TW_THEME := portrait_hdpi
+
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+
+TW_INTERNAL_STORAGE_PATH := "/data/media"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_DEFAULT_EXTERNAL_STORAGE := true
+TW_EXCLUDE_SUPERSU := true
+TW_NO_EXFAT_FUSE := true
+TW_INCLUDE_NTFS_3G := true
+TW_MTP_DEVICE := "/dev/mtp_usb"
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 162
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
+
+# TWRP encryption
+TW_CRYPTO_USE_SYSTEM_VOLD := true
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_SAMSUNG := true
+
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/13600000.usb/13600000.dwc3/gadget/lun%d/file"
+TARGET_SYSTEM_PROP := device/samsung/j7y17lte/system.prop
+
+# Color fix
+TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
+
